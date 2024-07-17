55f13bfc86b1e86f83019d847371401e41e1980d
haslam <haslamhdz71@gmail.com>
2024-07-17 08:42:30 +0700

    update schema sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -13,3 +13,30 @@ CREATE TABLE `role` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB
+
+CREATE TABLE `genre` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB
+
+CREATE TABLE `film` (
+  `id` varchar(124) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `genre_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `genre_id` (`genre_id`),
+  CONSTRAINT `film_ibfk_1` FOREIGN KEY (`genre_id`) REFERENCES `genre` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB
+
+CREATE TABLE `student_course` (
+  `student_id` varchar(100) NOT NULL,
+  `course_id` int NOT NULL
+) ENGINE=InnoDB
+
+CREATE TABLE `student` (
+  `id` varchar(100) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB
